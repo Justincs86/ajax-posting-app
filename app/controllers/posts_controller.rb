d0e95faafe3e7b0a7ddb1abbced1aac21fed595d
justin <justinng@Justins-MacBook-Air.local>
2017-06-08 16:53:41 +0800

    create post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:create, :destroy]
+
+  def index
+    @posts = Post.order("id DESC").all  #新的贴文放前面
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.save
+
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])   #只能删自己的贴
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+end
